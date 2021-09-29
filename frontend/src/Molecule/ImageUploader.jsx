969bfa0b0df0634c0fbc135e5f4da98dc8cbbece
Vishal Shinde <vcoolvish@yahoo.com>
2021-09-28 22:04:46 -0700

    Add ImageUploader component

---
+++ frontend/src/Molecule/ImageUploader.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { useStyletron } from 'baseui';
+import { FileUploader } from "baseui/file-uploader";
+import {Button, KIND, SHAPE} from 'baseui/button';
+import Upload from 'baseui/icon/upload';
+
+const ImageUploader = () => {
+  const [css] = useStyletron();
+  const [src, setSrc] = useState(null);
+
+  return (
+    <div className={css({
+      position: 'relative',
+      display: 'inline-flex'
+    })}>
+      <FileUploader
+        // onCancel={stopFakeProgress}
+        onDrop={(acceptedFiles) => {
+          setSrc(URL.createObjectURL(acceptedFiles[0]))
+        }}
+        overrides={{
+          FileDragAndDrop: {
+            style: () => ({
+              justifyContent: 'center',
+              minHeight: '200px',
+              minWidth: '200px',
+              width: '120px'
+            })
+          },
+          ContentMessage: {
+            component: () => src && (
+              <img
+                src={src}
+                className={css({
+                  width: '200px',
+                  height: 'auto',
+                  objectFit: 'contain'
+                })}
+              />
+            )
+          },
+          ButtonComponent: {
+            component: props => (
+              <Button {...props} kind={KIND.primary} shape={SHAPE.circle}>
+                <Upload />
+              </Button>
+            )
+          }
+        }}
+      />
+    </div>
+  );
+}
+
+export default ImageUploader;
