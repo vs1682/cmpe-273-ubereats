468ac43506e5c3115a78847c02807c300ae46063
Vishal Shinde <vcoolvish@yahoo.com>
2021-11-12 21:41:05 -0800

    Frontend: Make necessary changes in the initializer hooks

--- frontend/src/hooks/useFetchInitialDataForCustomer.jsx
+++ frontend/src/hooks/useFetchInitialDataForCustomer.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import _ from 'lodash';
 
 import { fetchCustomerProfile } from '../store/thunks/customer';
 import { fetchAllRestaurant } from '../store/thunks/restaurant';
+import { fetchOrderStatuses } from '../store/thunks/order';
 import { fetchDishCategories, fetchDishTypes } from '../store/thunks/dish';
 import { fetchDetails } from '../store/thunks/countries';
+import { setOrderFilter } from '../store/slices/filters';
 import { USER_TYPE } from '../utils/constants';
 
 const useFetchInitialDataForCustomer = () => {
@@ -12,6 +15,7 @@ const useFetchInitialDataForCustomer = () => {
   const user = useSelector(state => state.user);
   const profile = useSelector(state => state.customer.profile);
   const restaurantFilters = useSelector(state => state.filters.restaurant);
+  const orderStatuses = useSelector(state => state.order.statuses);
 
   useEffect(() => {
     if (user && user.accountRole === USER_TYPE.customer) {
@@ -19,8 +23,16 @@ const useFetchInitialDataForCustomer = () => {
       dispatch(fetchAllRestaurant({ custId: user.credId, filters: restaurantFilters }));
       dispatch(fetchDishCategories());
       dispatch(fetchDishTypes());
+      dispatch(fetchOrderStatuses());
     }
   }, [user && user.accountRole]);
+
+  useEffect(() => {
+    if (orderStatuses && orderStatuses.length > 0) {
+      const orderStatusMap = _.keyBy(orderStatuses, 'name');
+      dispatch(setOrderFilter(orderStatusMap['NEW'].id));
+    }
+  }, [orderStatuses]);
 
   useEffect(() => {
     if (profile && profile.credId) {
--- frontend/src/hooks/useFetchInitialDataForRestaurant.jsx
+++ frontend/src/hooks/useFetchInitialDataForRestaurant.jsx
@@ -1,23 +1,33 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import _ from 'lodash';
 
-import { fetchCustomerProfile } from '../store/thunks/customer';
+import { fetchOrderStatuses } from '../store/thunks/order';
 import { fetchRestaurant } from '../store/thunks/restaurant';
 import { fetchDishCategories, fetchDishTypes } from '../store/thunks/dish';
-import { fetchDetails } from '../store/thunks/countries';
+import { setOrderFilter } from '../store/slices/filters';
 import { USER_TYPE } from '../utils/constants';
 
 const useFetchInitialDataForRestaurant = () => {
   const dispatch = useDispatch();
   const user = useSelector(state => state.user);
+  const orderStatuses = useSelector(state => state.order.statuses);
 
   useEffect(() => {
     if (user && user.accountRole === USER_TYPE.restaurant) {
       dispatch(fetchRestaurant(user.credId));
       dispatch(fetchDishCategories());
       dispatch(fetchDishTypes());
+      dispatch(fetchOrderStatuses());
     }
   }, [user && user.accountRole]);
+
+  useEffect(() => {
+    if (orderStatuses && orderStatuses.length > 0) {
+      const orderStatusMap = _.keyBy(orderStatuses, 'name');
+      dispatch(setOrderFilter(orderStatusMap['NEW'].id));
+    }
+  }, [orderStatuses]);
 }
 
 export default useFetchInitialDataForRestaurant;
